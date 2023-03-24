f24a292be78a20b270370c000610697ed3b36fe9
AndreyKarpovich2321 <andrey.karpovich2321@gmail.com>
2023-03-24 14:30:20 +0300

    added Homework2ExtraTask class

---
+++ src/Homework2ExtraTask.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Homework2ExtraTask {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Username");
+        String userName = sc.nextLine();
+        System.out.println("Hello " + userName);
+
+    }
+}
