b4f2dd1a6178d3cb31cc42bf8fdf98011699f421
AndreyKarpovich2321 <andrey.karpovich2321@gmail.com>
2023-03-27 22:02:16 +0300

    Lesson3 class

---
+++ src/tms/karpovich/Lesson3/ThirdLessonClasswork.java
@@ -0,0 +1,38 @@
+package tms.karpovich.Lesson3;
+
+import java.util.Scanner;
+
+public class ThirdLessonClasswork {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter temperature");
+//        int temperature = scanner.nextInt();
+//        if (temperature > 20){
+//            System.out.println("Temperature is hot");
+//        }
+//        else if (temperature < 20 && temperature > 5){
+//            System.out.println("Temperature is warm");
+//        }
+//        else if (temperature > - 100 && temperature < 5){
+//            System.out.println("Temperature is cool");
+//        }
+//        for (int counter = 1; counter <= 100; counter++){
+//            int odd = counter%2 == 0 ? System.out.println(odd);
+//            if (counter%2 == 0) {
+//                System.out.println(counter)
+//            };
+//        int count = 1;
+//        while (count < 100){
+//            System.out.println(count);
+//            count += 2;}
+        for (int count = 0; count<=100; count+=5){
+
+                System.out.println(count);
+
+        }
+
+    }
+
+
+
+}
