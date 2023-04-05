80925034c684f6d4dcc73a2b433d768604a05d8f
AndreyKarpovich2321 <andrey.karpovich2321@gmail.com>
2023-04-05 21:58:10 +0300

    Fifth classwork

---
+++ src/tms/karpovich/lesson6/Car.java
@@ -0,0 +1,5 @@
+package tms.karpovich.lesson6;
+
+public class Car {
+
+}
---
+++ src/tms/karpovich/lesson6/SixthClasswork.java
@@ -0,0 +1,7 @@
+package tms.karpovich.lesson6;
+
+public class SixthClasswork {
+    public static void main(String[] args) {
+
+    }
+}
