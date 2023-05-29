39ff213299afeb225d736ee2810b5def2d05b766
AndreyKarpovich2321 <andrey.karpovich2321@gmail.com>
2023-05-29 21:50:44 +0300

    18

---
+++ src/tms/karpovich/lesson18/ArticleList.java
@@ -0,0 +1,15 @@
+package tms.karpovich.lesson18;
+
+import java.util.List;
+
+public class ArticleList {
+    private List<Article> articles;
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+}
